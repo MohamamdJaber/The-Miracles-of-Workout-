244d306b6831e64a31bd86fd25402d81c25dbe92
MohammadJaber <eng.mohammadjaber997@gmail.com>
2020-04-30 15:27:22 +0300

    lk

--- js/my java.js
+++ js/my java.js
@@ -1,7 +1,23 @@
-'use strict'
-console.log('My name is mohamamd');
-var age = 33;
-var name = 'xz';
-var lovecoding = true;
-var something;
-console.log (lovecoding);
+'use strict';
+var yourage = prompt('please enter your age');
+console.log(yourage);
+var okk;
+if (yourage > 18 && yourage < 60 ) {
+    confirm("Press enter..and dont forget to enjoy");
+    okk = 'Do any excercise you want you are healthy';
+  } else if (yourage > 12 && yourage < 60) {
+    confirm("Press enter..and dont forget to enjoy");
+    okk = 'You should wait until you become 18';
+  } else if (yourage >= 5 && yourage < 60) {
+    confirm("Tell your parents");
+    okk = 'Go to your BaBa';
+  } else {
+    confirm("Please read about working out within your age in the internet");
+    okk = 'Please dont even think to do that unless you are a fit person just eat a healthy stuff';
+  }
+  document.write('<h3>'+okk+'</h3>');
+  
+  document.write('<h3>'+"You can do it if you just try"+'</h3>'); 
+
+
+

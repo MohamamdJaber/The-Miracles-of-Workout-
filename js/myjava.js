ca2e500214acd085314fae10aea303dcfa4393c8
MohammadJaber <eng.mohammadjaber997@gmail.com>
2020-05-03 05:56:21 +0300

    yep

--- js/myjava.js
+++ js/myjava.js
@@ -1,10 +1,5 @@
 'use strict';
-function userOrder(){
-
-}
-
-
-
+{
 whatOrdering();
 height();
 age1();
@@ -12,21 +7,24 @@ function age1(){
     var yourage = prompt('please enter your age');
   console.log(yourage);
   var okk;
+  var how = prompt('how many days you want to excesice');
+  for (var i = 0; i < how;i++)
+  {
   if (yourage > 18 && yourage < 60 ) {
       confirm("Press enter..and dont forget to enjoy");
-      okk = 'Do any excercise you want you are healthy';
+      okk = 'Do any excercise you want you are healthy' + okk;
     } else if (yourage > 12 && yourage < 60) {
       confirm("Press enter..and dont forget to enjoy");
-      okk = 'You should wait until you become 18';
+      okk = 'You should wait until you become 18'+ okk;
     } else if (yourage >= 5 && yourage < 60) {
       confirm("Tell your parents");
-      okk = 'Go to your BaBa';
+      okk = 'Go to your BaBa'+ okk;
     } else {
       confirm("Please read about working out within your age in the internet");
-      okk = 'Please dont even think to do that unless you are a fit person just eat a healthy stuff';
-    }
+      okk = 'Please dont even think to do that unless you are a fit person just eat a healthy stuff'+ okk;
+    }}
     {
-    document.write('<h3>'+okk+'</h3>');}
+      document.write('<h3>'+okk+'</h3>');}
 }
 
 
@@ -53,17 +51,16 @@ function age1(){
     var whatOrdering = prompt('what your best excersice is');
     while (whatOrdering !== 'push ups' && whatOrdering !== 'not defind yet' )
 
-    {whatOrdering = prompt('please enter push ups or not defind yey ');}
+    {whatOrdering = prompt('please enter push ups or not defind yet ');}
    
     if (whatOrdering === 'push ups'){
       // document.write('<p>greaaaaat</p>');
       document.getElementById("new").innerHTML = "You are great";
     }else if (whatOrdering === 'not defind yet'){
-      document.write('<p>ok keep looking for that</p>');
+      document.getElementById("new").innerHTML = "ok keep looking for that";
     }
   }
 
-  
-  
+}
 
 
